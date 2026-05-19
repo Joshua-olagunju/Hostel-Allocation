ec40f749df22fc881e6574a2c32150bb75338a14
Abvakon-Mobile-Solutions <support@abvakonmobilesolutions.com>
2026-05-19 21:25:00 +0100

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+ 
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+       <section>
+        <div>
+          <p>Welcome to the App</p>
+        </div>
+       </section>
+    </>
+  )
+}
+
+export default App
